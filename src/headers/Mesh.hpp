16e5acae589b235047b3000e12418cf4a74ffb29
Jack Lomax <66711017+Jack-Lomax@users.noreply.github.com>
2021-08-24 21:09:47 +0100

    started mesh class

--- src/headers/Mesh.hpp
+++ src/headers/Mesh.hpp
@@ -6,6 +6,19 @@
 #include "VBO.hpp"
 #include "EBO.hpp"
 #include "Camera.hpp"
+#include "Texture.hpp"
+
+class Mesh
+{
+public:
+    std::vector <Vertex> vertices;
+    std::vector <GLuint> indices;
+    std::vector <Texture> texture;
+
+    VAO vao;
+
+    Mesh(std::vector<Vertex>& vertices, std::vector<GLuint>& indices, std::vector<Texture> textures);
+};
 
 
 #endif

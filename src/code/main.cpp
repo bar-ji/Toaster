0e9cbbe4cfc665356f7a2099dfceb95a83023e1b
Barji <66711017+BarjiYT@users.noreply.github.com>
2021-09-06 10:01:12 +0100

    Added ImGUI

--- src/code/main.cpp
+++ src/code/main.cpp
@@ -1,18 +1,12 @@
-#include <imGUI/imgui.h>
-#include <imGUI/imgui_impl_glfw.h>
-#include <imGUI/imgui_impl_opengl3.h>
-
+#include <imgui/imgui.h>
+#include <imgui/imgui_impl_glfw.h>
+#include <imgui/imgui_impl_opengl3.h>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
 #include "../headers/HelperFunctions.hpp"
-#include "../headers/Mesh.hpp"
 #include "../headers/Light.hpp"
-#include "../headers/Camera.hpp"
 #include "../headers/Model.hpp"
-#include "../headers/Shader.hpp"
 #include "../headers/Renderer.hpp"
 
 
